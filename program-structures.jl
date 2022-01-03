df88396571714c458cc47a5e2b06b1464e216c75
valentinaandrade <valentinaandrade@uchile.cl>
2022-01-03 01:24:32 -0300

    lear how to create a function

---
+++ program-structures.jl
@@ -0,0 +1,12 @@
+# Programming Structures
+
+# Functions
+# One-line
+myfunction1(var) = var+1
+# Several lines
+function myfunction2(var1, var2="Float64", var3=1)
+output1 = var1+2
+output2 = var2+4
+output3 = var3+3 # var3 is optional, by default var3=1
+return [output1 output2 output3]
+end
